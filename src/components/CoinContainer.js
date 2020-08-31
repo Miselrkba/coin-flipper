bc85fe2b8dc64eea552e31bca6e7802b8fab9b11
iii <miselrkba@gmail.com>
2020-08-31 22:01:29 +0200

    added comments

--- src/components/CoinContainer.js
+++ src/components/CoinContainer.js
@@ -3,12 +3,14 @@ import Coin from "./Coin";
 import { choice } from "./helpers";
 
 class CoinContainer extends Component {
+  //set deffault props
   static defaultProps = {
     coins: [
       { side: "heads", imgSrc: "https://tinyurl.com/react-coin-heads-jpg" },
       { side: "tails", imgSrc: "https://images.unsplash.com/photo-1561449898-65bb5a2ee943?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" }
     ]
   };
+  // set state withthese 4 values
   constructor(props) {
     super(props);
     this.state = {
@@ -20,11 +22,14 @@ class CoinContainer extends Component {
     this.handleClick = this.handleClick.bind(this);
   }
   flipCoin() {
+    //select random item from array
     const newCoin = choice(this.props.coins);
     this.setState(st => {
       return {
+        //set state to random item
         currCoin: newCoin,
         nFlips: st.nFlips + 1,
+        // if rand item is heads / tails add one or 0
         nHeads: st.nHeads + (newCoin.side === "heads" ? 1 : 0),
         nTails: st.nTails + (newCoin.side === "tails" ? 1 : 0)
       };
@@ -37,6 +42,7 @@ class CoinContainer extends Component {
     return (
       <div className='CoinContainer'>
         <h2>Let's Flip A Coin!</h2>
+        {/* at start state is empty after setState render coin */}
         {this.state.currCoin && <Coin info={this.state.currCoin} />}
         <button onClick={this.handleClick}>Flip Me!</button>
         <p>
